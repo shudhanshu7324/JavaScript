478c7e139347bd9d55014e4d7940d8dd33f79d8d
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-13 00:00:48 +0530

    variable in javascripts

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14453;
+let email = "shudhanshu@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+let accountStatus; // it will give undefined
+
+/*
+    donot use var keyword because of scoping problems
+*/
+
+// const cant be changed not allowed
+// accountId = 443242;
+
+email = "pandey@gmail.com"
+accountPassword = "4534534";
+accountCity = "Delhi"
+
+console.table([accountId,email,accountPassword,accountCity]);
