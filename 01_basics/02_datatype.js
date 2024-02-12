272a231d6cdb4f3ebbcd9ab0aa6c861990b40fa3
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-13 00:18:27 +0530

    datatypes in javascript

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all code as a newer javascript version
+
+// alert(3+3)  // we cannot use this in node because alert is an document method
+
+
+let age = 18;
+let name = "shudhanshu";
+let isLoggedIn = false;
+let address; // undefined
+let email = null;
+
+/* Primitive datatype
+number
+bigint
+string
+boolean
+null
+undefined
+
+Non Primitive
+object
+*/
+
+console.log(typeof age);
