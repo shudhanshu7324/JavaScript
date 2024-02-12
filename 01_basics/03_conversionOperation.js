7852a06985b4c4c840b9685bd70ffd8a04abb533
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-13 00:39:34 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let age = 33;
+console.log(typeof age);
+
+let age2 = "34abc";
+console.log(typeof age2);
+
+let value = Number(age2);
+console.log(typeof value);
+console.log(value);
+
+/*
+    "33" => 33
+    "33abc" => NaN
+    true => 1 , false => 0
+*/
+
+let loggedIn = 1;
+let booleanIsloggedin = Boolean(loggedIn);
+console.log(booleanIsloggedin);
