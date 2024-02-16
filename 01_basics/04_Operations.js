1a8ed41ee553ed3b11048bba1e4820dd96fc8235
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-16 17:23:53 +0530

    Operations in JavaScript

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,33 @@
+// Operations
+
+// let val = 3;
+// let negVal = -val;
+// console.log(negVal);
+
+// console.log(2 + 2); // adition operation
+// console.log(2 - 2); // substraction operation
+// console.log(2 * 2); // multiplication operation
+// console.log(2 / 2); // division operation
+// console.log(2 % 2); // modulus operation
+// console.log(2 ** 3); // power operation
+
+// let str1 = "hello ";
+// let str2 = "shudhanshu"
+
+// console.log(str1+str2); // string addition
+
+// tricky question in js
+console.log(
+  "1" + 2
+); /* if string first then all will be treated as string but if 1st it is number then string */
+console.log(1 + "2");
+
+console.log(1 + 2 + "2");
+console.log("1" + "2" + 2);
+
+// prefix operator and post fix operator
+let gameCounter = 100
+++gameCounter; // prefix operator
+console.log(gameCounter);
+gameCounter++; // postfix operator
+console.log(gameCounter);
