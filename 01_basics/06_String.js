5a21ab667085096b2ea3de9249d40a8be8276802
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-17 20:10:33 +0530

    strings in javascript

---
+++ 01_basics/06_String.js
@@ -0,0 +1,35 @@
+const name = "Shudhanshu";
+const value = 50;
+
+// console.log(name + " Value is: " + value); // normal way
+
+// string interpolation
+// console.log(`My name is ${name} and the value is ${value}`);
+
+const gameName = new String("hiteshhc");
+
+console.log(gameName);
+
+// console.log(typeof gameName);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+let gameName2 = gameName;
+// console.log(gameName2);
+
+gameName2 = "hello";
+
+console.log(gameName2);
+
+console.log(gameName);
+
+// .charAt()
+// .indexOf()
+// .substring()
+// .slice()
+// .trim()
+// .replace()
+// .includes()
+// .split('');
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,28 @@
+// primitive and non premitives
+
+// primitive
+/*
+    There are 7 types of primitive in js which all is call by value type
+    String
+    Number
+    Boolean
+    null
+    undefined
+    Symbol
+    BigInt
+*/
+
+// Symbol
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+// non primitive / reference type
+/*
+    Array
+    Objects
+    Functions
+*/
+
+// Javascript is dynamically typed language
