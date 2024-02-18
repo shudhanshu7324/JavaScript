cf639f8f947560138f8cf5282e806a1d1e4a43dc
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-18 16:09:01 +0530

    date and time in js

---
+++ 01_basics/08_DateAndTime.js
@@ -0,0 +1,16 @@
+let date = new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toISOString());
+// console.log(date.toDateString());
+
+// console.log(date.toLocaleString());
+
+// console.log(typeof date);
+
+// define date by yourself
+
+// let myCreatedDate = new Date(2024, 0, 23);
+// let myCreatedDate = new Date(2024, 0, 23, 5, 4);
+// let myCreatedDate = new Date(2024, 0, 23, 5, 4);
+// let myCreatedDate = new Date("01-05-2020");
