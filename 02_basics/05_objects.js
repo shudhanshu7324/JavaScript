f3feba9248c1e147b95bf3de22909dbbbfbbab9e
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-20 19:19:54 +0530

    object final part

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,15 @@
+// destructuring of objects
+const course = {
+  courseName: "js-hindi",
+  price: "999",
+  courseInstructor: "hitesh",
+};
+
+// course.courseInstructor
+
+// destructuring
+const { courseInstructor, courseName, price } = course;
+
+// console.log(courseInstructor);
+// console.log(courseName);
+// console.log(price);
