eab3c66529db5dc8964b1e4f79b8adc2a2df705c
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-22 19:43:17 +0530

    scoping in js

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,71 @@
+if (true) {
+  // let a = 5;
+  // const b = 6;
+  var c = 7;
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+// global variable is accessed inside any block but local variale is not accessed outside of the block.
+
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
