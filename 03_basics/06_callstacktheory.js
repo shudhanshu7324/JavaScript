dd1d0e7bdf15255d0082109945afa33e18239168
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-24 17:17:35 +0530

    call stack and execution

---
+++ 03_basics/06_callstacktheory.js
@@ -0,0 +1,15 @@
+/* 
+    JavaScript execution context
+
+    {} -> Global Execution context  <- this (everyone have their own context like in browser window object)
+
+    Function execution context
+
+    Eval execution context 
+
+    {} -> it run in two stage
+    1. Memory creation phase
+    2. Execution phase
+
+    
+*/
