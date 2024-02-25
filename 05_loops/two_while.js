8b4689f09af4b174e9795cdb1d489755d6b04cc6
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-25 14:52:14 +0530

    while and do while loop

---
+++ 05_loops/two_while.js
@@ -0,0 +1,29 @@
+// while(condition){
+//     body
+// }
+
+// let index = 1;
+// while(index<=10){
+//     console.log(index);
+//     index++;
+// }
+
+// let index = 1;
+// while(index++){
+//     console.log(index);
+//     if(index == 10){
+//         break;
+//     }
+// }
+
+// do while loop
+
+// do{
+
+// }while(condition)
+
+let score = 1;
+do{
+    console.log(score);
+    score++;
+}while(score<=10);
