9ed3758326273b90716047752c3a2dabc33f2acb
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-25 22:11:53 +0530

    for of loops on object

---
+++ 06_advanceloops/loopsonObject.js
@@ -0,0 +1,21 @@
+const myObj = {
+  js: "javascript",
+  cpp: "C++",
+  py: "python",
+};
+
+// for in loops
+
+// for (const key in myObj) {
+//   console.log(key, " ", myObj[key]);
+// }
+
+const programming = ["js", "java", "c++", "python"];
+
+// for(const key in programming){
+//     console.log(key);
+// }
+
+for(const key in programming){
+    console.log(programming[key]);
+}
