e79d64ee845454c8cc79f6393da899bfcc724ec8
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-02-26 00:26:27 +0530

    array reduce method

--- 06_advanceloops/higherOrderloops.js
+++ 06_advanceloops/higherOrderloops.js
@@ -41,3 +41,48 @@
 // userBooks.forEach((item) => {
 //     console.log(item.title);
 // })
+
+// Map method -> it doesnot modify the original array
+const myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNum = myNumber.map((num) => num + 10);
+console.log(newNum);
+
+console.log(myNumber);
+
+// Reduce method
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
